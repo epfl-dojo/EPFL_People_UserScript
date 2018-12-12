2b8852ce68f58ee6d5a509f65319e110f7f6659b
Nicolas Borboën <ponsfrilus@gmail.com>
2018-12-12 11:14:25 +0100

    Meta data improvements

--- EPFL_People.user.js
+++ EPFL_People.user.js
@@ -1,15 +1,21 @@
 // ==UserScript==
 // @name        EPFL People
 // @namespace   none
-// @version     1.3.1
+// @version     1.3.2
+// @author      EPFL-dojo
 // @description A script to improve browsing on people.epfl.ch
 // @include     https://people.epfl.ch/*
 // @include     https://personnes.epfl.ch/*
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
-// @author      EPFL-dojo
-// @downloadURL https://raw.githubusercontent.com/epfl-dojo/EPFL_People_UserScript/master/EPFL_People.user.js
+// @homepage    https://github.com/ponsfrilus/EPFL_People_UserScript/
+// @homepageURL https://github.com/ponsfrilus/EPFL_People_UserScript/
+// @hwebsite    https://github.com/ponsfrilus/EPFL_People_UserScript/
+// @source      https://github.com/ponsfrilus/EPFL_People_UserScript/
+// @downloadURL https://github.com/ponsfrilus/EPFL_People_UserScript/raw/master/EPFL_People.user.js
+// @updateURL   https://github.com/ponsfrilus/EPFL_People_UserScript/raw/master/EPFL_People.user.js
+// @supportURL  https://github.com/ponsfrilus/EPFL_People_UserScript/issues
 // @icon        https://github.com/ponsfrilus/EPFL_People_UserScript/raw/master/img/epfl_search_people.png
 // @iconURL     https://github.com/ponsfrilus/EPFL_People_UserScript/raw/master/img/epfl_search_people.png
 // @defaulticon https://github.com/ponsfrilus/EPFL_People_UserScript/raw/master/img/epfl_search_people.png
