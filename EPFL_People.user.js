6647f388eb173ce3c94c780471c2f617deedca2f
Nicolas Borboën <ponsfrilus@gmail.com>
2020-05-20 15:43:24 +0200

    [comfort] open admin data by default

--- EPFL_People.user.js
+++ EPFL_People.user.js
@@ -4,7 +4,7 @@
 // @description A script to improve browsing on people.epfl.ch
 // @include     https://people.epfl.ch/*
 // @include     https://personnes.epfl.ch/*
-// @version     1.3
+// @version     1.3.1
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
 // @require     https://code.jquery.com/jquery-3.5.1.min.js
@@ -28,6 +28,9 @@ $(document).ready(function () {
 
   // Add sciper after name in title
   $("#main > div.container > div.d-flex.flex-wrap.justify-content-between.align-items-baseline > h1").append(" #" + getScipterFromOnload())
+
+  // Comfort, open adminstrative data by default
+  $('span:contains("Administrative data")').parent().click()
 
 });
 
