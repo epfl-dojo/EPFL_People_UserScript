adcf5a731f5246e2e658328076bce731f7abfd05
Nicolas Reymond <n.reymond.12@gmail.com>
2020-05-27 17:55:34 +0200

    [F.T] Use people-API to get some user's info

--- EPFL_People.user.js
+++ EPFL_People.user.js
@@ -4,9 +4,13 @@
 // @description A script to improve browsing on people.epfl.ch
 // @include     https://people.epfl.ch/*
 // @include     https://personnes.epfl.ch/*
+// @include     https://search.epfl.ch//*
+// @include     https://search-api.epfl.ch//*
 // @version     1.3.2
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
+// @grant       GM.setValue
+// @grant       GM.getValue
 // @require     https://code.jquery.com/jquery-3.5.1.min.js
 // @author      EPFL-dojo
 // @downloadURL https://raw.githubusercontent.com/epfl-dojo/EPFL_People_UserScript/master/EPFL_People.user.js
@@ -19,25 +23,27 @@
 // TODO: [ ] get the mailinglist
 // TODO: [ ] add proper meta data on the phone number
 
-$(document).ready(function () {
+$(document).ready(async function () {
 
-  function getScipterFromOnload() {
-    var re = /sciper=([0-9]{6})/;
-    try {
-      var sciper = ($("html").html().match(re)[1])
-      console.log(sciper)
-    } catch (e) {
-      throw new Error("No sciper found")
-    }
-    return sciper
+  async function getInfosFromPeopleAPI() {
+    var peopleAPI = 'https://search-api.epfl.ch/api/ldap?q=' + document.title
+    GM_xmlhttpRequest({
+      method: "GET",
+      url: peopleAPI,
+      onload: async function (response) {
+        let info = $.parseJSON(response.responseText)
+        await GM.setValue('user', info[0])
+      }
+    })
   }
 
-  let sciper = getScipterFromOnload()
+  await getInfosFromPeopleAPI()
+  let user = await GM.getValue('user')
+  console.log(user)
   let username = $('dt:contains("Username")').next('dd').html()
-  let unit = $('[itemprop="address"] > strong').html()
 
   // Add sciper after name in title
-  $("#main > div.container > div.d-flex.flex-wrap.justify-content-between.align-items-baseline > h1").append(" #" + sciper);
+  $("#main > div.container > div.d-flex.flex-wrap.justify-content-between.align-items-baseline > h1").append(" #" + user.sciper);
 
   // Comfort, open admindata by default
   unsafeWindow.toggleVis('admin-data')
@@ -47,8 +53,10 @@ $(document).ready(function () {
   $('<div class="d-flex flex-wrap justify-content-between align-items-baseline" id="EPFLPeopleUserScriptData"></div>').insertAfter(".container:first div:first");
   $('#EPFLPeopleUserScriptData').css('font-family', 'monospace')
   $('#EPFLPeopleUserScriptData').css('white-space', 'pre')
-  $('#EPFLPeopleUserScriptData').append('<div>sciper: ' + sciper + '</div>')
+  $('#EPFLPeopleUserScriptData').append('<div>sciper: ' + user.sciper + '</div>')
   $('#EPFLPeopleUserScriptData').append('<div>username: ' + username + '</div>')
-  $('#EPFLPeopleUserScriptData').append('<div>unit: ' + unit + '</div>')
+  $('#EPFLPeopleUserScriptData').append('<div>unit: ' + user.accreds[0].path + '</div>')
+
+
 
 });
